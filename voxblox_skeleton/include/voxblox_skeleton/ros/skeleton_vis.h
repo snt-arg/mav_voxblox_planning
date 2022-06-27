a3132883ad046d21b8024fed9dd46883b8f60c99
hriday <hriday.bavle@gmail.com>
2022-06-28 00:08:32 +0200

    Add code for getting all connected layers

--- voxblox_skeleton/include/voxblox_skeleton/ros/skeleton_vis.h
+++ voxblox_skeleton/include/voxblox_skeleton/ros/skeleton_vis.h
@@ -12,6 +12,50 @@
 
 namespace voxblox {
 
+struct connected_vertices_struct { 
+public:
+  int64_t id;
+  bool visited;
+  SkeletonVertex vertex;
+  int64_t subgraph_id;
+};
+
+inline void connected_components(int64_t subgraph_id, const SparseSkeletonGraph& graph, connected_vertices_struct& connected_vertex, std::vector<connected_vertices_struct>& connected_vertices_struct_vec, std::vector<int64_t> closed_space_edge_ids) {
+  std::vector<int64_t> edge_list = connected_vertex.vertex.edge_list;
+  //std::cout << "connected_vertex.vertex.vertex_id " << connected_vertex.vertex.vertex_id << std::endl;
+  for(int64_t edge_id : edge_list) {
+    auto it_edge = std::find(closed_space_edge_ids.begin(), closed_space_edge_ids.end(), edge_id);
+    //edge exists 
+    if(it_edge != closed_space_edge_ids.end()) {
+      const SkeletonEdge& connected_edge = graph.getEdge(edge_id);
+
+      if(connected_edge.start_vertex != connected_vertex.vertex.vertex_id) {
+        //this is the vertex neighbour        
+        auto start_vertex = std::find_if(connected_vertices_struct_vec.begin(), connected_vertices_struct_vec.end(), boost::bind(&connected_vertices_struct::id, _1) == connected_edge.start_vertex);
+        if(start_vertex != connected_vertices_struct_vec.end() && (*start_vertex).visited == false) {
+          (*start_vertex).visited = true;
+          (*start_vertex).subgraph_id = subgraph_id;
+          connected_components(subgraph_id, graph, (*start_vertex), connected_vertices_struct_vec, closed_space_edge_ids);
+        }
+        else 
+         continue;  
+      }  
+      else if (connected_edge.end_vertex != connected_vertex.vertex.vertex_id) {  
+        //this is the vertex neighbour
+        auto end_vertex = std::find_if(connected_vertices_struct_vec.begin(), connected_vertices_struct_vec.end(), boost::bind(&connected_vertices_struct::id, _1) == connected_edge.end_vertex);
+        if(end_vertex != connected_vertices_struct_vec.end() && (*end_vertex).visited == false) {
+          (*end_vertex).visited = true;
+          (*end_vertex).subgraph_id = subgraph_id;
+          connected_components(subgraph_id, graph, (*end_vertex), connected_vertices_struct_vec, closed_space_edge_ids);
+        }
+        else 
+          continue;  
+      }
+    }
+  }
+  return; 
+}
+
 inline void visualizeSkeletonGraph(
     const SparseSkeletonGraph& graph, const std::string& frame_id,
     visualization_msgs::MarkerArray* marker_array) {
@@ -21,7 +65,10 @@ inline void visualizeSkeletonGraph(
   // Get a list of all vertices and visualize them as spheres.
   std::vector<int64_t> vertex_ids;
   graph.getAllVertexIds(&vertex_ids);
+  std::vector<int64_t> deleted_vertex_ids;
+  std::vector<connected_vertices_struct> connected_vertices_struct_vec;
 
+  visualization_msgs::Marker closed_space_marker;
   visualization_msgs::Marker vertex_marker;
   vertex_marker.points.reserve(vertex_ids.size());
   // Also create the free-space marker.
@@ -29,10 +76,15 @@ inline void visualizeSkeletonGraph(
 
   vertex_marker.header.frame_id = frame_id;
   vertex_marker.ns = "vertices";
+  closed_space_marker.header.frame_id = frame_id;
+  closed_space_marker.ns = "closed_spaces";
+
   if (!visualize_subgraphs) {
     vertex_marker.type = visualization_msgs::Marker::SPHERE_LIST;
+    closed_space_marker.type = visualization_msgs::Marker::SPHERE_LIST;
   } else {
     vertex_marker.type = visualization_msgs::Marker::CUBE_LIST;
+    closed_space_marker.type = visualization_msgs::Marker::CUBE_LIST;
   }
 
   vertex_marker.pose.orientation.w = 1.0;
@@ -41,6 +93,13 @@ inline void visualizeSkeletonGraph(
   vertex_marker.scale.z = vertex_marker.scale.x;
   vertex_marker.color.r = 1.0;
   vertex_marker.color.a = 1.0;
+
+  closed_space_marker.pose.orientation.w = 1.0;
+  closed_space_marker.scale.x = 0.2;
+  closed_space_marker.scale.y = closed_space_marker.scale.x;
+  closed_space_marker.scale.z = closed_space_marker.scale.x;
+  closed_space_marker.color.r = 1.0;
+  closed_space_marker.color.a = 1.0;
 
   vertex_free_space_marker.header.frame_id = frame_id;
   vertex_free_space_marker.type = visualization_msgs::Marker::SPHERE;
@@ -55,15 +114,34 @@ inline void visualizeSkeletonGraph(
     tf::pointEigenToMsg(vertex.point.cast<double>(), point_msg);
     vertex_marker.points.push_back(point_msg);
 
+    connected_vertices_struct connected_vertex;
+    if (vertex.distance > 0.8) {
+      
+      closed_space_marker.points.push_back(point_msg);
+      connected_vertex.id = vertex_id;
+      connected_vertex.visited = false;
+      connected_vertex.vertex = vertex;
+      connected_vertices_struct_vec.push_back(connected_vertex);
+    }
+
     if (visualize_subgraphs) {
       std_msgs::ColorRGBA color_msg;
-      Color color = grayColorMap(vertex.subgraph_id % 100 / 10.0);
+      Color color = rainbowColorMap(vertex.subgraph_id % 100 / 10.0);
       colorVoxbloxToMsg(color, &color_msg);
-      vertex_marker.colors.push_back(color_msg);
+      if(vertex.distance > 0.8) {
+        vertex_marker.colors.push_back(color_msg);
+        closed_space_marker.colors.push_back(color_msg);
+      }
+      else {
+        color_msg.b = 0; color_msg.r = 0; color_msg.g = 0;
+        vertex_marker.colors.push_back(color_msg);
+        deleted_vertex_ids.push_back(vertex_id);
+      }
     }
 
     if (visualize_freespace) {
-      if (vertex.distance > 1e-1) {
+      //std::cout << "vertex distance: " << vertex.distance << std::endl;
+      if (vertex.distance > 0.8) {
         vertex_free_space_marker.pose.orientation.w = 1.0;
         vertex_free_space_marker.pose.position = point_msg;
         vertex_free_space_marker.scale.x = vertex.distance;
@@ -74,11 +152,12 @@ inline void visualizeSkeletonGraph(
       }
     }
   }
-
+  marker_array->markers.push_back(closed_space_marker);
   marker_array->markers.push_back(vertex_marker);
 
   // Get all edges and visualize as lines.
   std::vector<int64_t> edge_ids;
+  std::vector<int64_t> closed_space_edge_ids;
   graph.getAllEdgeIds(&edge_ids);
 
   visualization_msgs::Marker edge_marker;
@@ -98,15 +177,34 @@ inline void visualizeSkeletonGraph(
   for (int64_t edge_id : edge_ids) {
     geometry_msgs::Point point_msg;
     const SkeletonEdge& edge = graph.getEdge(edge_id);
+    int64_t start_vertex_id = edge.start_vertex;
+    int64_t end_vertex_id = edge.end_vertex;
+    
+    auto it_start = std::find(deleted_vertex_ids.begin(), deleted_vertex_ids.end(), start_vertex_id);
+    auto it_end = std::find(deleted_vertex_ids.begin(), deleted_vertex_ids.end(), end_vertex_id);
+
+    if(it_start != deleted_vertex_ids.end() || it_end != deleted_vertex_ids.end()) 
+      continue;
+
+    closed_space_edge_ids.push_back(edge_id);
     tf::pointEigenToMsg(edge.start_point.cast<double>(), point_msg);
     edge_marker.points.push_back(point_msg);
     tf::pointEigenToMsg(edge.end_point.cast<double>(), point_msg);
     edge_marker.points.push_back(point_msg);
   }
-
   marker_array->markers.push_back(edge_marker);
-}
 
+  int64_t subgraph_id = 0;
+  for(connected_vertices_struct connected_vertex : connected_vertices_struct_vec) {
+    if(connected_vertex.visited == false) {
+      connected_vertex.visited = true;
+      connected_vertex.subgraph_id = subgraph_id;
+      connected_components(subgraph_id, graph, connected_vertex, connected_vertices_struct_vec, closed_space_edge_ids);
+      subgraph_id++;      
+    }    
+  }
+  std::cout << "subgraph id: " << subgraph_id << std::endl; 
+ }
 }  // namespace voxblox
 
 #endif  // VOXBLOX_SKELETON_ROS_SKELETON_VIS_H_
